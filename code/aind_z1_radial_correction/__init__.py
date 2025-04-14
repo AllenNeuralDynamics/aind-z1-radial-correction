a1b22607e9ad4d9ce493fbf15ff066246787673e
Camilo Laiton <36769694+camilolaiton@users.noreply.github.com>
2025-04-14 10:32:51 -0700

    refactoring project to match code ocean standards and installation

---
+++ code/aind_z1_radial_correction/__init__.py
@@ -0,0 +1,21 @@
+"""Z1 radial correction."""
+
+__version__ = "0.0.1"
+__authors__ = ["Carson Berry", "Camilo Laiton", "Tim Wang"]
+__author_emails__ = [
+    "carson.berry@alleninstitute.org",
+    "camilo.laiton@alleninstitute.org",
+    "tim.wang@alleninstitute.org",
+]
+__license__ = "MIT"
+__description__ = "Image preprocessing for radial correction"
+__url__ = "https://github.com/AllenNeuralDynamics/aind-z1-radial-correction"
+
+__maintainers__ = ["Carson Berry", "Camilo Laiton"]
+__maintainer_emails__ = [
+    "carson.berry@alleninstitute.org",
+    "camilo.laiton@alleninstitute.org",
+]
+__title__ = "aind-z1-radial-correction"
+__status__ = "Development"  # 'Development' 'Production', 'Beta'
+__pipeline_version__ = "0.0.1"
---
+++ code/aind_z1_radial_correction/radial_correction.py
@@ -0,0 +1,9 @@
+"""
+Computes radial correction in microscopic data
+"""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
